4ffb4d3e75617a9f6f8a2ff2b28bbd60074fdaef
gabrielmsato <sato031297@gmail.com>
2021-05-03 20:12:17 -0300

    Update server.c

--- server.c
+++ server.c
@@ -10,11 +10,17 @@ int main() {
         printf("Nao foi possivel a criacao do server socket\n");
         return 1;
     }
-    printf("Server socket criado com sucesso");
+    printf("Server socket criado com sucesso\n");
 
     // Configurando o socketdo servidor 
     server.sin_family = AF_INET; 
 	server.sin_addr.s_addr = INADDR_ANY; 
 	server.sin_port = htons(PORTNO); 
+
+    if (bind(serverfd, (struct sockaddr *) &server , sizeof(server)) < 0) {
+        printf("Nao foi possivel fazer o bind do server socket\n");
+        return 1;
+    }
+    printf("Bind feito com sucesso\n");
     return 0;
 }
